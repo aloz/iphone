b7f377c1fbf1ea6dc44203f76e4fd176b253f453
aloz <alozovsky@CentMobiles-MacBook-Air.local>
2013-10-26 18:36:51 +0300

    test commit 2

---
+++ task1/task1/labAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  labAppDelegate.h
+//  task1
+//
+//  Created by Anton Lozovsky on 10/26/13.
+//  Copyright (c) 2013 Anton Lozovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface labAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
