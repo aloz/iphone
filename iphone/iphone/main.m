50e3299fb4b106e25cd3267a8d099eff304836aa
aloz <alozovsky@CentMobiles-MacBook-Air.local>
2013-10-26 18:28:38 +0300

    test commit

---
+++ iphone/iphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iphone
+//
+//  Created by Anton Lozovsky on 10/26/13.
+//  Copyright (c) 2013 Anton Lozovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "labAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([labAppDelegate class]));
+    }
+}
